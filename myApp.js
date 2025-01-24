bb08640e9bcc67b588b89a43cf9c6e4675b55ad3
George Datcu <143513063+gdatcu@users.noreply.github.com>
2025-01-24 14:33:33 +0000

    Add new things

--- myApp.js
+++ myApp.js
@@ -1,6 +1,41 @@
-let express = require('express');
+require('dotenv').config();
+let express = require('express')
 let app = express();
 
-console.log("Hello World");
+app.use('/', function name(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
+GET /:word/echo. Respond with a JSON object, taking the structure {echo: word}. You can find the word to be repeated at req.params.word.
+
+app.get(':word/echo', function(req, res))
+
+// Chain middleware to the route "/now"
+app.get('/now', function(req, res, next) {
+  // Middleware function to add current time to req.time
+  req.time = new Date().toString();
+  next(); // Pass control to the next handler
+}, function(req, res) {
+  // Final handler to respond with the JSON object
+  res.json({ time: req.time });
+});
+
+/* app.get('/json', function(req, res) {
+res.json(process.env.MESSAGE_STYLE === 'uppercase' ? {"message": "HELLO JSON"}: {"message": "Hello json"});
+});
+*/
+
+/* app.get('/json', function(req, res) {
+  res.json({"message": "Hello json"})
+});
+*/
+
+/* app.get('/', function(req, res){
+  const absolutePath = __dirname + "/views/index.html";  res.sendFile(absolutePath)
+});
+*/
+
+
+app.use('/public', express.static(__dirname + "/public"));
 module.exports = app;
